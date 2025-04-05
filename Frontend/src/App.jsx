79fe8d85594aef728142d6be6c06b3f31bb53d5e
sanskar22082002 <sanskarkashyap156@gmail.com>
2025-04-05 22:55:26 +0530

    Super Admin Home File Created- (Sanskar)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Home/Home";
 import React, { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
+import SuperAdminHome from "./Home/SuperAdminHome";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/SuperAdminHome" element={<SuperAdminHome />} />
         </Routes>
     </>
   );
---
+++ Frontend/src/Home/SuperAdminHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SuperAdminHome() {
+  return (
+    <div>
+      <h1>HI Abhi </h1>
+    </div>
+  )
+}
+
+export default SuperAdminHome
