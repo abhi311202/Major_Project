2b955b7afd972e101a4d4bdc03bdaca1a5afb1cf
sanskar22082002 <sanskarkashyap156@gmail.com>
2025-04-14 09:48:14 +0530

    Your message here

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,11 @@ import React, { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import UserLogin from "./components/UserLogin";
 import UserSignUp from "./components/UserSignUp";
+import AdminLogin from "./components/AdminLogin";
+import AdminSignUp from "./components/AdminSignUp";
+import SuperAdminLogin from "./components/SuperAdminLogin";
+
+import AdminHome from "./Home/AdminHome";
 
 
 const App = () => {
@@ -12,8 +17,13 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} /> {/* ✅ Add this line */}
+          <Route path="/AdminHome" element={<AdminHome />} />
           <Route path="/UserLogin" element={<UserLogin />} />
           <Route path="/UserSignUp" element={<UserSignUp />} />
+          <Route path="/AdminLogin" element={<AdminLogin />} />
+          <Route path="/AdminSignUp" element={<AdminSignUp />} />
+          <Route path="/SuperAdminLogin" element={<SuperAdminLogin />} />
+          
         </Routes>
 
     </>
