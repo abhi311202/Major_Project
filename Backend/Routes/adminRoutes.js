500d2d89163adbfb89559124cef77b20143c56e3
abhi311202 <abhinitnaware01@gmail.com>
2025-05-03 01:19:58 +0530

    Backend Added agaiin -(abhi)

--- Backend/Routes/adminRoutes.js
+++ Backend/Routes/adminRoutes.js
@@ -16,12 +16,8 @@ import { DocUpload } from "../Controllers/DocumentController.js";
 router.post("/register", registerAdmin);
 router.post("/login", adminLogin);
 router.post("/logout", logout);
-<<<<<<< HEAD
 router.post("/UploadDocument", DocUpload);
-=======
-router.post("/adminDocuments", uploadDocs);
-router.post("/adminDocumentsUploaded", getDocs);
->>>>>>> 2aba28a227849b55085c9d632265d58eb505a7e5
+
 // router.post("/verify", userMiddleware, demo);
 
 export default router;
